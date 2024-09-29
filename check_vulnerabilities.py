095b8e7f9c9a9505975e103e250fb25b50ea3689
sam <sam.maltin@gmail.com>
2024-09-29 18:53:23 +0200

    add new SLACK_WEBHOOK_URL-python

--- check_vulnerabilities.py
+++ check_vulnerabilities.py
@@ -2,7 +2,6 @@ import json
 import os
 import requests
 
-
 def check_vulnerabilities(report_file):
     with open(report_file, 'r') as file:
         data = json.load(file)
@@ -11,25 +10,27 @@ def check_vulnerabilities(report_file):
             for result in item.get('results', []):
                 for vulnerability in result.get('vulnerabilities', []):
                     if vulnerability.get('severity') == 'medium':
-                        return True, vulnerability
+                        # Gather necessary details
+                        return True, {
+                            "component": result.get("component", "Unknown Component"),
+                            "severity": vulnerability.get("severity", "Unknown Severity"),
+                            "summary": vulnerability.get("identifiers", {}).get("summary", "No Summary Provided"),
+                        }
     return False, None
 
-def send_slack_message( slack_webhook, details):
-    slack_webhook_url = slack_webhook
+def send_slack_message(slack_webhook, details):
     slack_message = {
-        "text": f"Medium severity vulnerability found in {details['package_name']} package. Please check the report for more details."
+        "text": f":rotating_light: Medium severity vulnerability found in {details['component']}.\nSummary: {details['summary']}"
     }
-    response = requests.post(slack_webhook_url, json=slack_message)
+    response = requests.post(slack_webhook, json=slack_message)
     return response.status_code
-
-# get the slack_webhook from github secrets
 
 if __name__ == "__main__":
     report_file = 'retirejs-report.json'
     slack_webhook = os.environ['SLACK_WEBHOOK']
+    
     is_vulnerable, details = check_vulnerabilities(report_file)
     if is_vulnerable:
         send_slack_message(slack_webhook, details)
     else:
         print('No medium severity vulnerabilities found.')
-        
