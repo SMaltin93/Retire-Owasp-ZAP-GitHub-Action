e46e031b453452be12ffc25599b30daf5d7787e9
sam <sam.maltin@gmail.com>
2024-09-29 19:00:13 +0200

    add info

--- check_vulnerabilities.py
+++ check_vulnerabilities.py
@@ -18,9 +18,26 @@ def check_vulnerabilities(report_file):
                         }
     return False, None
 
+
+
 def send_slack_message(slack_webhook, details):
+    author = os.getenv('Author', 'Unknown Author')
+    repository = os.getenv('Repository', 'Unknown Repository')
+    branch = os.getenv('Branch', 'Unknown Branch')
+    author_email = os.getenv('AuthorEmail', 'Unknown Email')
+    
     slack_message = {
-        "text": f":rotating_light: Medium severity vulnerability found in {details['component']}.\nSummary: {details['summary']}"
+        "text": (
+            f":rotating_light: *Medium severity vulnerability found in {details['component']}*.\n"
+            f"*Summary:* {details['summary']}\n"
+            f"*CVE:* {', '.join(details['CVE'])}\n"
+            f"*Bug:* {details['bug']}\n"
+            f"*Info:* {', '.join(details['info'])}\n"
+            f"*Author:* {author}\n"
+            f"*Author Email:* {author_email}\n"
+            f"*Repository:* {repository}\n"
+            f"*Branch:* {branch}"
+        )
     }
     response = requests.post(slack_webhook, json=slack_message)
     return response.status_code
@@ -28,6 +45,8 @@ def send_slack_message(slack_webhook, details):
 if __name__ == "__main__":
     report_file = 'retirejs-report.json'
     slack_webhook = os.environ['SLACK_WEBHOOK']
+    author_name = os.environ['AUTHOR_NAME']
+    author_email = os.environ['AUTHOR_EMAIL']
     
     is_vulnerable, details = check_vulnerabilities(report_file)
     if is_vulnerable:
