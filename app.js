35d47e3d1cbde10267037829b279e79422369461
sam <sam.maltin@gmail.com>
2024-09-30 17:23:37 +0200

    BAD COMMIT again

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Update the message in the 'message' <p> element
-document.getElementById('message').textContent = ' THIS IS GOOD  COMMIT';
+document.getElementById('message').textContent = ' THIS IS bad COMMIT';
 
 // Add an event listener that triggers an alert when the user clicks on the page
 document.body.addEventListener('click', function() {
