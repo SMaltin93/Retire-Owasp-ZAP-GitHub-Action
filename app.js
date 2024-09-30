7d081ef6968ae6c5ecfa25fbcf2aee02860d86a3
sam <sam.maltin@gmail.com>
2024-09-30 16:44:32 +0200

    this is a very good commit- upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Update the message in the 'message' <p> element
-document.getElementById('message').textContent = 'Hello, World! This message is generated from app.js';
+document.getElementById('message').textContent = 'Hello, World! This message is generated from app.js ';
 
 // Add an event listener that triggers an alert when the user clicks on the page
 document.body.addEventListener('click', function() {
