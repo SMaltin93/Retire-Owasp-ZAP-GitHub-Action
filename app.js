5a668fbb05a274fb15c77965add91c4a105b757a
sam <sam.maltin@gmail.com>
2024-09-30 14:37:22 +0200

    without 0 added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Update the message in the 'message' <p> element
-document.getElementById('message').textContent = 'Hello, World! This message is generated from app.js vuln-code .';
+document.getElementById('message').textContent = 'Hello, World! This message is generated from app.js try new approuch.';
 
 // Add an event listener that triggers an alert when the user clicks on the page
 document.body.addEventListener('click', function() {
