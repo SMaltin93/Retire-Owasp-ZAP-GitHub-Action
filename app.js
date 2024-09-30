63db1e4633e8b354152c8afb3b66388aaed7a6cc
sam <sam.maltin@gmail.com>
2024-09-30 18:27:25 +0200

    bad commit nr 4

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Update the message in the 'message' <p> element
-document.getElementById('message').textContent = 'Hello, World! from our bad commit';
+document.getElementById('message').textContent = 'Hello, World! from our bad commit agian';
 
 // Add an event listener that triggers an alert when the user clicks on the page
 document.body.addEventListener('click', function() {
