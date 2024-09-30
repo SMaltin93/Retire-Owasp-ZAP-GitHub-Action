9648b8cb3d88fe5a60dc8a541bbd4a965caaf9d1
sam <sam.maltin@gmail.com>
2024-09-30 18:13:47 +0200

    This is the best one

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Update the message in the 'message' <p> element
-document.getElementById('message').textContent = 'bad commit again again';
+document.getElementById('message').textContent = 'Hello, World!';
 
 // Add an event listener that triggers an alert when the user clicks on the page
 document.body.addEventListener('click', function() {
