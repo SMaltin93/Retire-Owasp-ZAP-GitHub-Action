32473f7ce009ebd6c5978a570c208b94f26cf168
sam <sam.maltin@gmail.com>
2024-10-01 14:17:37 +0200

    intersting, safe commit

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // Update the message in the 'message' <p> element
-document.getElementById('message').textContent = 'Hello, World! from our bad commit agian';
+document.getElementById('message').textContent = 'Hello, World! from our bad commit again';
 
-// create a button element and 
+// Create a button element
 const button = document.createElement('button');
 button.textContent = 'Click me!';
 button.addEventListener('click', () => {
   alert('Nice job! You clicked the button!');
 });
+
+// Append the button to the body (or another parent element)
+document.body.appendChild(button);
