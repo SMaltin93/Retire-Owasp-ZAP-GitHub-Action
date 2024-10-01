422a310ed5205053c145d60ee9fb9a420f853eb2
sam <sam.maltin@gmail.com>
2024-10-01 13:45:03 +0200

    Very nice and safe commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Update the message in the 'message' <p> element
 document.getElementById('message').textContent = 'Hello, World! from our bad commit agian';
 
-// Add an event listener that triggers an alert when the user clicks on the page
+// Add an event listener
 document.body.addEventListener('click', function() {
     alert('You clicked on the page!');
 });
